909bb6b0bd3ad03794b91c131464a10a66c551ed
Tom Fleming <tf@Toms-Mac-mini.local>
2023-10-21 20:42:14 +0100

    added initial collector code

---
+++ collector/collector.py
@@ -0,0 +1,39 @@
+import time
+import paho.mqtt.client as mqtt
+import json
+import weatherhat
+
+# MQTT setup
+MQTT_HOST = "10.0.0.41"
+MQTT_PORT = 1883
+MQTT_KEEPALIVE_INTERVAL = 45
+MQTT_TOPIC = "v1/weatherstation/data"
+
+# Define on_publish event function
+def on_publish(client, userdata, mid):
+    print("Message Published...")
+
+# Initiate MQTT Client
+mqttc = mqtt.Client()
+# Connect with MQTT Broker
+mqttc.connect(MQTT_HOST, MQTT_PORT, MQTT_KEEPALIVE_INTERVAL)
+
+# Configure weatherhat
+sensor = weatherhat.WeatherHat()
+sensor.update(interval=5.0)
+
+while True:
+    # Read sensor data
+    data = {
+        "sys_temperature": sensor.device_temperature, # celsius
+        "temperature": sensor.temperature, # celsius
+        "humidity": sensor.humidity, # %
+        "pressure": sensor.pressure, # hPa
+        "light": sensor.lux,
+        "wind_speed": sensor.wind_speed, # mph
+        "wind_direction": sensor.wind_direction, # degrees
+        "rain_tota": sensor.rain_total, # mm
+        "rain": sensor.rain, # mm/sec
+    }
+    mqttc.publish(MQTT_TOPIC, json.dumps(data))
+    time.sleep(5.0)
